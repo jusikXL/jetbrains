e5c472c9b572d3c636ae0ad6394ab4ddd118a856
jusikXL <125464973+jusikXL@users.noreply.github.com>
2025-03-07 10:41:06 +0100

    refactor: separate client from main

---
+++ src/client.rs
@@ -0,0 +1,99 @@
+use std::{
+    error::Error,
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpStream,
+    ops::Range,
+};
+
+const KB: u32 = 1024;
+
+pub struct DataConfig {
+    len: u32, // could have utilized 'Content-Range' header, but it is not provided by the server
+    hash: String,
+}
+impl DataConfig {
+    pub fn new(len: u32, hash: String) -> Self {
+        Self { len, hash }
+    }
+}
+
+pub struct ServerConfig {
+    addr: String,
+}
+impl ServerConfig {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+}
+
+pub struct Client {
+    server_config: ServerConfig,
+    data_config: DataConfig,
+}
+
+impl Client {
+    const CHUNK_SIZE: u32 = 64 * KB;
+
+    pub fn new(server_config: ServerConfig, data_config: DataConfig) -> Self {
+        Self {
+            server_config,
+            data_config,
+        }
+    }
+
+    pub fn run(&self) -> Result<(), Box<dyn Error>> {
+        let mut data: Vec<u8> = Vec::new(); // aggregated data
+
+        let mut start = 0;
+        let len = self.data_config.len;
+        while start < len {
+            let end = len.min(start + Self::CHUNK_SIZE);
+
+            // (re)connect as server closes connection
+            let mut stream = TcpStream::connect(&self.server_config.addr)?;
+
+            // send request
+            let request = Self::get_request(start..end);
+            stream.write_all(request.as_bytes())?;
+
+            // receive response
+            let response_body = Self::read_response(&mut stream)?;
+            data.extend_from_slice(&response_body);
+
+            // move to the next chunk
+            start += Self::CHUNK_SIZE;
+        }
+
+        assert_eq!(data.len() as u32, len, "length mismatch");
+        assert_eq!(sha256::digest(data), self.data_config.hash, "hash mismatch");
+
+        println!("Success");
+        Ok(())
+    }
+
+    fn get_request(range: Range<u32>) -> String {
+        println!("{}-{}", range.start, range.end - 1);
+        format!(
+            "GET / HTTP/1.1\r\nRange: bytes={}-{}\r\n\r\n",
+            range.start, range.end
+        )
+    }
+
+    fn read_response(stream: &mut TcpStream) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut reader = BufReader::new(stream);
+
+        // skip headers
+        loop {
+            let mut line = String::new();
+            reader.read_line(&mut line)?;
+            if line == "\r\n" {
+                break;
+            }
+        }
+
+        let mut body = Vec::new();
+        reader.read_to_end(&mut body)?;
+
+        Ok(body)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,20 @@
+pub mod client;
+
 use std::{
-    error::Error,
-    io::{self, BufRead, BufReader, Read, Write},
-    net::TcpStream,
-    ops::Range,
+    io::{self},
     process,
 };
 
+use client::{Client, DataConfig, ServerConfig};
+
 const SERVER_ADDR: &str = "127.0.0.1:8080";
-const KB: u32 = 1024;
-const CHUNK_SIZE: u32 = 64 * KB;
 
-struct DataConfig {
-    len: u32, // could have utilized 'Content-Range' header, but it is not provided by the server
-    hash: String,
-}
-
-struct ServerConfig {
-    addr: String,
-}
-
-struct Client {
-    server_config: ServerConfig,
-    data_config: DataConfig,
-}
-
-impl Client {
-    pub fn new(server_config: ServerConfig, data_config: DataConfig) -> Self {
-        Self {
-            server_config,
-            data_config,
-        }
-    }
-
-    pub fn run(&self) -> Result<(), Box<dyn Error>> {
-        let mut data: Vec<u8> = Vec::new(); // aggregated data
-
-        let mut start = 0;
-        let len = self.data_config.len;
-        while start < len {
-            let end = len.min(start + CHUNK_SIZE);
-
-            // (re)connect as server closes connection
-            let mut stream = TcpStream::connect(&self.server_config.addr)?;
-
-            // send request
-            let request = Self::get_request(start..end);
-            stream.write_all(request.as_bytes())?;
-
-            // receive response
-            let response_body = Self::read_response(&mut stream)?;
-            data.extend_from_slice(&response_body);
-
-            // move to the next chunk
-            start += CHUNK_SIZE;
-        }
-
-        assert_eq!(data.len() as u32, len, "length mismatch");
-        assert_eq!(sha256::digest(data), self.data_config.hash, "hash mismatch");
-
-        println!("Success");
-        Ok(())
-    }
-
-    fn get_request(range: Range<u32>) -> String {
-        println!("{}-{}", range.start, range.end - 1);
-        format!(
-            "GET / HTTP/1.1\r\nRange: bytes={}-{}\r\n\r\n",
-            range.start, range.end
-        )
-    }
-
-    fn read_response(stream: &mut TcpStream) -> Result<Vec<u8>, Box<dyn Error>> {
-        let mut reader = BufReader::new(stream);
-
-        // skip headers
-        loop {
-            let mut line = String::new();
-            reader.read_line(&mut line)?;
-            if line == "\r\n" {
-                break;
-            }
-        }
-
-        let mut body = Vec::new();
-        reader.read_to_end(&mut body)?;
-
-        Ok(body)
-    }
-}
+// TODO: introduce concurrency
 
 fn main() {
     let data_config = read_config();
 
-    println!(
-        "Fetching data of length {} with hash {}",
-        data_config.len, data_config.hash
-    );
-
-    let client = Client::new(
-        ServerConfig {
-            addr: SERVER_ADDR.to_string(),
-        },
-        data_config,
-    );
+    let client = Client::new(ServerConfig::new(SERVER_ADDR.to_string()), data_config);
 
     if let Err(e) = client.run() {
         eprintln!("Application error: {e}");
@@ -148,5 +60,7 @@ fn read_config() -> DataConfig {
         }
     };
 
-    DataConfig { len, hash }
+    println!("Fetching data of length {} with hash {}", len, hash);
+
+    DataConfig::new(len, hash)
 }
